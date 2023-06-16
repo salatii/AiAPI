726845a244acf10221311945f4913731ba163642
salatii <katioderso@gmail.com>
2023-06-16 12:54:16 +0200

    add basic structore for controller

---
+++ src/main/java/main/api/controller/AiController.java
@@ -0,0 +1,28 @@
+package main.api.controller;
+
+import main.api.DataStructure.Article;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AiController {
+    @GetMapping("/rss")
+    public String getRss() {
+        return "rss";
+    }
+
+    @GetMapping("/translation")
+    public String getTranslation(Article body) {
+        return "translation";
+    }
+
+    @GetMapping("/keywords")
+    public String getKeywords(Article body) {
+        return "keywords";
+    }
+
+    @GetMapping("/summary")
+    public String getSummary(Article body) {
+        return "summary";
+    }
+}
