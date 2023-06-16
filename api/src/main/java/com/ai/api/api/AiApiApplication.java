cadc22d9991b24c4adfb5e943a7e6e57f9b0b922
salatii <katioderso@gmail.com>
2023-06-16 11:32:04 +0200

    add Spring Boot base project

---
+++ api/src/main/java/com/ai/api/api/AiApiApplication.java
@@ -0,0 +1,13 @@
+package com.ai.api.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/ai/api/api/AiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.api.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
