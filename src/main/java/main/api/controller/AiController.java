01b25661139df1d1cadfb7ff8188b443b4f5e708
kh24 <katjana-1.herbst@uni-ulm.de>
2023-06-16 20:56:10 +0200

    add first rss controller mapping

--- src/main/java/main/api/controller/AiController.java
+++ src/main/java/main/api/controller/AiController.java
@@ -1,27 +1,70 @@
 package main.api.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import main.api.Manager;
 import main.api.data.Article;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class AiController {
-    @GetMapping("/rss")
-    public String getRss() {
-        return "rss";
+    @RequestMapping("/main")
+    public String getMain() {
+        Manager manager = new Manager("main", "arg1", "arg2", "arg3");
+        try {
+            List<String> result = manager.runPython();
+            result.forEach(entry -> System.out.println(entry));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "main";
     }
 
-    @GetMapping("/translation")
-    public String getTranslation(Article body) {
+    @RequestMapping("/rss")
+    public String getRss() {
+        Manager manager = new Manager("rss");
+        JSONArray nodes = new JSONArray();
+        try {
+            List<String> result = manager.runPython();
+            result.forEach(entry -> {
+                ObjectMapper mapper = new ObjectMapper();
+                try {
+                    JsonNode node = mapper.readTree(entry.replace("\"", "").replace('\'', '\"'));
+                    nodes.put(node);
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return nodes.toString();
+    }
+
+    @RequestMapping(value="/translation", method = RequestMethod.GET)
+    public String getTranslation(@RequestParam("language") Optional<String> t_language, Article body) {
         return "translation";
     }
 
-    @GetMapping("/keywords")
+    @RequestMapping("/keywords")
     public String getKeywords(Article body) {
         return "keywords";
     }
 
-    @GetMapping("/summary")
+    @RequestMapping("/summary")
     public String getSummary(Article body) {
         return "summary";
     }
